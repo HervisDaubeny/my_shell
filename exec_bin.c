5da129a5927e1488e688eb877be42f10e3508e21
HervisDaubeny <uhlas47@gmail.com>
2019-12-26 16:31:23 +0100

    Update: exec_bin.c - First implementation.

--- exec_bin.c
+++ exec_bin.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int exec_bin(**char binary) {
+  int fork_val = fork()
+  int exec_val = 0;
+
+  switch (fork_val) {
+    case 0:
+      //In child process
+      exec_val = execvp(binary);
+      break;
+    case -1:
+      //Error while forking happend, do something appropriate
+      break;
+    default :
+      //In parrent, handle exec_val
+      break;
+  }
+}
