e5447e38b8b0e88a206d66eac954f5dba44ae895
HervisDaubeny <uhlas47@gmail.com>
2019-12-26 23:08:54 +0100

    update: exec_bin.c - Works with fixed binary[] argument.

--- exec_bin.c
+++ exec_bin.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-int exec_bin(**char binary) {
-  int fork_val = fork()
-  int exec_val = 0;
+#define PATH "/bin:/usr/bin"
 
-  switch (fork_val) {
-    case 0:
-      //In child process
-      exec_val = execvp(binary);
-      break;
-    case -1:
-      //Error while forking happend, do something appropriate
-      break;
-    default :
-      //In parrent, handle exec_val
-      break;
-  }
+int exec_bin(char** binary) {
+	pid_t pid = fork();
+	int exec_val = 0;
+
+	if(pid == 0) {
+		//In child process
+	    	if (execvp(binary[0], binary) < 0) {
+			if (errno == 2) {
+				//TODO: cover known errnos
+			}
+
+			printf("Couldn't execute, errno: %d\n", errno);
+		}
+		exit(0);
+	}
+	else if (pid == -1) {
+		return 1;
+	}
+	else {
+		wait(NULL);
+	}
+
+	return exec_val;
+}
+
+int main() {
+	 char* binary[] = {"s", "-l", "-a", NULL};
+	 int res = 0;
+
+	 res = exec_bin(binary);
+
+	 return 0;
 }
