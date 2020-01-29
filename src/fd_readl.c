a31e5323a07fdfb28fbaee7914fd1eb03c82bb76
HervisDaubeny <uhlas47@gmail.com>
2020-01-29 15:36:25 +0100

    Update: Memmory work, return types and method contents improved.

--- src/fd_readl.c
+++ src/fd_readl.c
@@ -2,19 +2,50 @@
 #include <stdlib.h>
 #include <sys/types.h>                   
 #include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "utils.h"
 
 int fd_open(char* file) {
-	int fd = (int)open(file, O_RDONLY);
+	int fd = open(file, O_RDONLY);
+	if (fd == -1) {
+		char* buff;
+		char* mess;
+		
+		MALLOC(buff, 128);
+		mess = "Shelly: error while opening file:";
+		sprintf(buff, "%s %d\n", mess, errno);
+		write(STDERR_FILENO, buff, strlen(buff));
+
+		FREE(buff);
+		exit(1);
+	}
 
 	return fd;
 }
 
 int fd_close(int fd) {
-	return close(fd);
+	int result = close(fd);
+	if (result == -1) {
+		char* buff;
+		char* mess;
+		
+		MALLOC(buff, 128);
+		mess = "Shelly: error while closing file:";
+		sprintf(buff, "%s %d\n", mess, errno);
+		write(STDERR_FILENO, buff, strlen(buff));
+
+		FREE(buff);
+		exit(1);
+	}
+
+	return result;
 }
 
+/*
 char fd_getc(int fd) {
 	static int size = 10;
 	static int poss = 0;
@@ -23,6 +54,10 @@ char fd_getc(int fd) {
 	char ret = '\0';
 
 	if(poss >= chread) {
+		chread = 0;
+		for(int i = 0; i < size; i++) {
+			buffer[i] = '\0';
+		}
 		chread = (int)read(fd, buffer, size);
 		poss = 0;
 
@@ -37,12 +72,27 @@ char fd_getc(int fd) {
 
 	return ret;
 }
+*/
 
+char fd_getc(int fd) {
+	char buffer[1];
+	ssize_t red = 0;
+	ssize_t eof = 0;
+	const int size = 1;
+
+	red = read(fd, buffer, size);
+	if(red == eof) {
+		return -1;
+	}
+
+	return *buffer;
+}
 char* fd_getl(int fd, int* eof) {
-	int size = 80;
+	size_t size = 80;
 	int length = 0;
-	char* buffer = malloc(size);
+	char* buffer; 
 	char* line;
+	MALLOC(buffer, size);
 
 	while(1) {
 		char c = fd_getc(fd);
@@ -52,26 +102,23 @@ char* fd_getl(int fd, int* eof) {
 			*eof = 1;
 		}
 
-		if(length >= size - 1) {
+		if(length >= (int)size - 1) {
 			size *= 2;
-			if((realloc(buffer, size)) == NULL) {
-					printf("Error in malloc!\n");
-					exit(1);
-			}
+			RELLOC(buffer, size);
 		}
 
 		buffer[length] = c;
 		length ++;
 
 		if(c == '\n' || *eof) {
-			line = malloc(length + 1);
+			MALLOC(line, (length + 1));
 
 			for(int i = 0; i < length; i++) {
-				line[i] = buffer[i];
+				line [i] = buffer[i];
 			}
 			line[length] = '\0';
 
-			free(buffer);
+			FREE(buffer);
 			return line;
 		}
 	}
@@ -92,7 +139,7 @@ int main(int argc, char* argv[]) {
 	while(1) {
 		char* line = fd_getl(fd, &eof);
 		printf("%s", line);
-		free(line);
+		FREE(line);
 		if(eof) {
 			break;
 		}
